82d1edc77e05b470d79645d5873a6df5376081e2
Sergey Petrinich <petrinichshadow@gmail.com>
2022-09-16 16:07:59 +0300

    bug fixing

--- src/_components/AppRouter.tsx
+++ src/_components/AppRouter.tsx
@@ -2,6 +2,7 @@ import { useEffect, FC } from "react";
 import { useLocation, useRoutes } from "react-router-dom";
 import dynamicAdaptive from "../helpers/dynamic_adapt";
 import { spollers, _closeAllSpollers } from "../helpers/functions";
+import { useEffectOnce } from "../hooks";
 import { routesConfig } from "../routes";
 import useMenu from "../store/slices/menu/useMenu";
 
@@ -29,6 +30,21 @@ const PageChange: FC = () => {
 
 const AppRouter: FC = () => {
     const routes = useRoutes(routesConfig);
+
+    useEffectOnce(() => {
+        const onPageLoad = () => {
+            document.documentElement.classList.add("_loaded");
+        };
+
+        // Check if the page has already loaded
+        if (document.readyState === "complete") {
+            onPageLoad();
+        } else {
+            window.addEventListener("load", onPageLoad);
+            // Remove the event listener when component unmounts
+            return () => window.removeEventListener("load", onPageLoad);
+        }
+    });
 
     return (
         <>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useWindowSize } from "./useWindowSize";
 export { default as useClickOutside } from "./useClickOutside";
 export { default as useEventListener } from "./useEventListener";
 export { default as useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
+export { default as useEffectOnce } from "./useEffectOnce";
